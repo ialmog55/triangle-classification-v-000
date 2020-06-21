ece0bbfcba05828fe8e989e0de98fffe7ce854a5
idith almog <almog.idith@gmail.com>
2020-06-21 18:07:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,26 @@
 class Triangle
-  attr_accessor :kind, :isosceles
+  attr_accessor :a,:b, :c, :isosceles, :equilateral, :scalene
   class TriangleError < StandardError
   end
   def initialize(a,b,c)
-    if a == b && b == c
-    @kind = @isosceles
+    @a=a
+    @b=b
+    @c=c  
+  end
+  def kind
+    if !(@a > 0 && @b >0 && @c >0 && @a+@b >@c && @a+@c >@b && @b+@c > @a)
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b && @b != @c
+      :isosceles
+    elsif @b == @c
+      :isosceles
+    elsif @a == @c
+      :isosceles
+    else
+      :scalene
     end
   end
-
-
+  
   end
