4d24273eb9550a5a5c5117f1e7a03bd51850cd16
idith almog <almog.idith@gmail.com>
2020-06-21 17:39:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,12 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
   class TriangleError < StandardError
   end
-end
+  def initialize(a,b,c)
+    if a == b && b == c
+    @kind = isosceles
+    end
+  end
+
+
+  end
